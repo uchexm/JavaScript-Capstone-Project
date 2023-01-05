f91551d37e40e17117404b59592debac6edd4551
uchexm <109084069+uchexm@users.noreply.github.com>
2023-01-05 20:28:18 +0200

    Add README.md

--- src/modules/getComment.js
+++ src/modules/getComment.js
@@ -1,30 +1,28 @@
 export default async (id) => {
-  console.log("test", id);
   try {
     await fetch(
-      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zzwfsFxqWArAT5ak4r3D/comments?item_id=${id}`
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/zzwfsFxqWArAT5ak4r3D/comments?item_id=${id}`,
     )
       .then((response) => response.json())
       .then((json) => {
-        console.log(json.length);
         document.getElementById(
-          "comment-header"
+          'comment-header',
         ).innerHTML = `COMMENTS (${json.length}) `;
-        document.getElementById("comments-container").innerHTML = "";
+        document.getElementById('comments-container').innerHTML = '';
         json.forEach((i) => {
           const comment = `${i.creation_date} ${i.username}: ${i.comment}`;
-          const commentElement = document.createElement("p");
+          const commentElement = document.createElement('p');
           commentElement.innerText = comment;
 
           // Append the comment to the comments container
 
           document
-            .getElementById("comments-container")
+            .getElementById('comments-container')
             .appendChild(commentElement);
         });
       });
   } catch {
-    document.getElementById("comments-container").innerHTML = "";
-    document.getElementById("comment-header").innerHTML = `COMMENTS (0) `;
+    document.getElementById('comments-container').innerHTML = '';
+    document.getElementById('comment-header').innerHTML = 'COMMENTS (0) ';
   }
 };
